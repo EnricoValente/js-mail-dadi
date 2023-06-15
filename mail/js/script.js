47a15f39dba477edb558c340e7048bc77e1ef24b
EnricoValente <enricovalente42@gmail.com>
2023-06-15 14:59:48 +0200

    Inizializzazione DOM e crazione form

---
+++ mail/js/script.js
@@ -0,0 +1,23 @@
+// Mail
+// Chiedi all'utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull'esito del controllo.
+
+
+/* 
+1) creo un form per chiedere all'utente la sua email
+2) creo un array con una lista di email
+    -SE l'mail dell'utente corrisponde a una delle email scritte nella lista,allora può accedere
+    -ALTRIMENTI non può accedere
+3) stampo un messaggio sull'esito di controllo 
+
+*/
+
+
+
+
+
+
+
+
+
